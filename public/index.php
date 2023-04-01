fb1c4fee8fbf893df3975c5da3b65de2d16f68de
Rkay Ji <mrrkayj86@gmail.com>
2023-04-01 19:43:22 +0530

    same code message

--- public/index.php
+++ public/index.php
@@ -2,9 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 0);
 error_reporting(E_ALL & ~E_NOTICE);
-use App\core\Application;
-$loader = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
-$loader->addPsr4('APP\\', __DIR__.'/../src/');
+use app\core\Application;
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
 $app = new Application(dirname(__DIR__));
 
